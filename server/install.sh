34791a926b5e956c06500b83b1b1e7d7fa4ddbd2
Jón Tómas Grétarsson <jontg@ulfhedinn.net>
2013-01-19 23:40:55 -0800

    more groundwork

--- server/install.sh
+++ server/install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 sudo ln -s /home/jontg/Development/kindle-weather/server/logrotate /etc/logrotate.d/kindle-weather
+
+cat <<-EOF | EDITOR=">" crontab -e
+	55 * * * * /home/jontg/Development/kindle-weather/server/weather-script.sh >> /home/jontg/log/weather-script.log 2>&1
+EOF
