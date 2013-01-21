69d3e0b28c04afa8928945ce96df83cee13f8216
Jon T Gretarsson <jon@relateiq.com>
2013-01-21 08:34:15 -0800

    moving logic around, adding a template for the banner

--- server/lib/KindleWeather.rb
+++ server/lib/KindleWeather.rb
@@ -4,6 +4,8 @@ require 'yaml'
 require 'net/http'
 require 'uri'
 require 'xmlsimple'
+
+# Temporary requirements
 require 'pp'
 
 class KindleWeather
@@ -11,8 +13,9 @@ class KindleWeather
 	ROOT = File.expand_path(File.join(File.dirname(__FILE__), "..")) # Get the root directory	
 	CONFIG_FILE = "#{ROOT}/config/KindleWeather.conf"
 	WEATHER_PANEL_SMALL = ERB.new File.new("#{ROOT}/templates/kindle-weather-small.erb").read
+	WEATHER_PANEL_BANNER = ERB.new File.new("#{ROOT}/templates/kindle-weather-banner.erb").read
 	WEATHER_URL = ERB.new <<-EOU
-		http://graphical.weather.gov/xml/SOAP_server/ndfdSOAPclientByDay.php?whichClient=NDFDgenByDay&lat=<%= @latitude %>&lon=<%= @longitude %>&format=24+hourly&numDays=4&Unit=e
+		http://graphical.weather.gov/xml/SOAP_server/ndfdSOAPclientByDay.php?whichClient=NDFDgenByDay&lat=<%= @latitude %>&lon=<%= @longitude %>&format=24+hourly&numDays=5&Unit=e
 	EOU
 
 	attr_accessor :latitude, :longitude
@@ -23,16 +26,19 @@ class KindleWeather
 		@longitude = settings["longitude"]
 	end
 
-	def process
+	def fetch_and_parse_5day
 		puts print @latitude, "\t", @longitude
-
 		weather_uri = URI.parse(WEATHER_URL.result(binding).lstrip)
 		weather_info = XmlSimple.xml_in(Net::HTTP.get(weather_uri), {'ForceArray' => false, 'KeyAttr' => ['type']})
 		day_of_week = weather_info["data"]["time-layout"][0]["start-valid-time"].map{|s| Date::DAYNAMES[Date.strptime(s, "%Y-%m-%d").wday]}
 		icons = weather_info["data"]["parameters"]["conditions-icon"]["icon-link"].map{|elem| URI.parse(elem).path.split('/')[-1][/[a-zA-Z_]*/]}
 		highs = weather_info["data"]["parameters"]["temperature"]["maximum"]["value"]
 		lows = weather_info["data"]["parameters"]["temperature"]["minimum"]["value"]
-		pp day_of_week.zip(icons,highs, lows)
+		day_of_week.zip(icons,highs, lows)
+	end
 
+	def process
+		weather_5day = fetch_and_parse_5day()
+		pp weather_5day
 	end
 end # class
