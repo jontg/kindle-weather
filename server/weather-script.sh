52d2dfac920f025608ba21dffbf46ecb955abd2f
Jón Tómas Grétarsson <jontg@ulfhedinn.net>
2013-11-25 12:23:20 -0800

    Switch to the (now working) ruby version

--- server/weather-script.sh
+++ server/weather-script.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"
 
-python2 weather-script.py
+ruby bin/kindle-weather
 rsvg-convert --background-color=white -o weather-script-output-pre.png weather-script-output.svg
 pngcrush -c 0 -ow weather-script-output-pre.png weather-script-output.png
 rcp weather-script-output.png root@192.168.2.2:/mnt/us/weather/weather-uploaded-output.png >/dev/null
