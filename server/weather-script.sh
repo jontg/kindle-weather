a1d860956cadc390d4f53793b2e2708d84766eaa
Jón Tómas Grétarsson <jon.gretarsson@gmail.com>
2015-06-28 11:29:33 -0700

    Dockerize all the things

--- server/weather-script.sh
+++ server/weather-script.sh
@@ -3,6 +3,7 @@
 cd "$(dirname "$0")"
 
 ruby bin/kindle-weather
-rsvg-convert --background-color=white -o weather-script-output-pre.png weather-script-output.svg
-pngcrush -c 0 -ow weather-script-output-pre.png weather-script-output.png
-rcp weather-script-output.png root@192.168.2.2:/mnt/us/weather/weather-uploaded-output.png >/dev/null
+
+rsvg-convert --background-color=white -o output/weather-script-output-pre.png output/weather-script-output.svg
+curl -X POST -s --form "input=@output/weather-script-output-pre.png;type=image/png" http://pngcrush.com/crush > output/weather-script-output.png
+# rcp output/weather-script-output.png root@192.168.2.2:/mnt/us/weather/weather-uploaded-output.png >/dev/null
