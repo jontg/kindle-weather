9a0ef181be93ac63d2aca383b61e1310ea55364d
Jón Tómas Grétarsson <jon.gretarsson@gmail.com>
2015-06-28 23:06:30 -0700

    Misc. Fixes

--- server/weather-script.sh
+++ server/weather-script.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
+#!/bin/bash -ex
 
 cd "$(dirname "$0")"
 
 ruby bin/kindle-weather
 
+cp weather-script-icons.svg output/
 rsvg-convert --background-color=white -o output/weather-script-output-pre.png output/weather-script-output.svg
-curl -X POST -s --form "input=@output/weather-script-output-pre.png;type=image/png" http://pngcrush.com/crush > output/weather-script-output.png
-# rcp output/weather-script-output.png root@192.168.2.2:/mnt/us/weather/weather-uploaded-output.png >/dev/null
+
+pushd output
+    pngcrush -c 0 weather-script-output-pre.png weather-script-output.png
+popd
+
+if [[ ! "${DO_DEPLOY}" -eq "" ]]; then
+    rcp output/weather-script-output.png root@192.168.2.2:/mnt/us/weather/weather-uploaded-output.png >/dev/null
+fi
