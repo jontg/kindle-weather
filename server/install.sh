f7fd9960bd318981a80e0c8a44488a61a25d695c
Jón Tómas Grétarsson <jon.gretarsson@gmail.com>
2015-07-07 00:12:23 -0700

    Fix

--- server/install.sh
+++ server/install.sh
@@ -3,5 +3,5 @@
 sudo ln -s /home/jontg/Development/kindle-weather/server/logrotate /etc/logrotate.d/kindle-weather
 
 cat <<-EOF | EDITOR=">" crontab -e
-	55 * * * * /home/jontg/Development/kindle-weather/server/weather-script.sh >> /home/jontg/log/weather-script.log 2>&1
+	55 * * * * DO_DEPLOY=true /home/jontg/Development/kindle-weather/server/weather-script.sh >> /home/jontg/log/weather-script.log 2>&1
 EOF
--- server/weather-script.sh
+++ server/weather-script.sh
@@ -11,6 +11,6 @@ pushd output
     pngcrush -c 0 weather-script-output-pre.png weather-script-output.png
 popd
 
-if [[ ! "${DO_DEPLOY}" -eq "" ]]; then
+if [[ ! -z "${DO_DEPLOY}" ]]; then
     rcp output/weather-script-output.png root@192.168.2.2:/mnt/us/weather/weather-uploaded-output.png >/dev/null
 fi
